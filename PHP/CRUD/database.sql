7b707b6fffc63a3ed1ef7956187b8f54526b79f7
jnavarromarti <57764974+jnavarromarti@users.noreply.github.com>
2024-12-05 10:10:55 +0100

    Delete PHP directory

--- PHP/CRUD/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE contacts_db;
-USE contacts_db;
-
-CREATE TABLE contacts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    phone VARCHAR(15) NOT NULL
-);
