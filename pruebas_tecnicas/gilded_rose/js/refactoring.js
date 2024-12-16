c06cefde15307baab9a8d62e452ea64e463aaf35
jnavarromarti <jnavarromarti0@uoc.edu>
2024-12-16 21:17:39 +0100

    solucion del Gildl Rose refactoring kata para JS jasmine

--- pruebas_tecnicas/gilded_rose/js/refactoring.js
+++ pruebas_tecnicas/gilded_rose/js/refactoring.js
@@ -1,68 +1,84 @@
 // sin modificar la clase item
 class Item {
-    constructor(name, sellIn, quality){
-      this.name = name;
-      this.sellIn = sellIn;
-      this.quality = quality;
-    }
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
   }
-  
-  class Shop {
-    constructor(items=[]){
-      this.items = items;
-    }
-    updateQuality() {
-      for (var i = 0; i < this.items.length; i++) {
-        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.items[i].quality > 0) {
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-            if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-              if (this.items[i].sellIn < 11) {
-                if (this.items[i].quality < 50) {
-                  this.items[i].quality = this.items[i].quality + 1;
-                }
-              }
-              if (this.items[i].sellIn < 6) {
-                if (this.items[i].quality < 50) {
-                  this.items[i].quality = this.items[i].quality + 1;
-                }
-              }
-            }
-          }
-        }
-        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          this.items[i].sellIn = this.items[i].sellIn - 1;
-        }
-        if (this.items[i].sellIn < 0) {
-          if (this.items[i].name != 'Aged Brie') {
-            if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-              if (this.items[i].quality > 0) {
-                if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                  this.items[i].quality = this.items[i].quality - 1;
-                }
-              }
-            } else {
-              this.items[i].quality = this.items[i].quality - this.items[i].quality;
-            }
-          } else {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
-            }
-          }
-        }
+}
+
+class Shop {
+  constructor(items = []) {
+    this.items = items;
+  }
+  // "Aged Brie" "Backstage passes to a TAFKAL80ETC concert" "Sulfuras, Hand of Ragnaros" "Conjured Mana Cake"
+  // item.name items.sellIn item.quality
+  // nueva solucion
+  updateQuality() {
+    for (var i = 0; i < this.items.length; i++) {
+      let item = this.items[i]
+
+      switch (item.name) {
+        case "Sulfuras, Hand of Ragnaros":
+
+          continue;
+        case "Aged Brie":
+          updateBrie(item)
+          break;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          updatePasses(item)
+          break;
+        case item.name.toLowerCase().includes("conjured"):
+          updateConjured(item)
+          break;
+        default:
+          udateNormalItem(item)
+          break;
       }
-  
-      return this.items;
+    }
+    return this.items;
+  }
+  updateBrie(item) {
+    item.quality = Math.min(50, item.quality + 1)
+    item.sellIn--
+    if (item.sellIn < 0) {
+      item.quality = Math.min(50, item.quality + 2)
+      item.sellIn--
     }
   }
-  module.exports = {
-    Item,
-    Shop
+  updatePasses(item) {
+    if (item.sellIn < 0) {
+      item.quality = 0
+      item.sellIn--
+    } else {
+      item.quality = + 1
+      if (item.sellIn < 10) {
+        item.quality = Math.min(50, item.quality + 2)
+        item.sellIn--
+      } else if (item.sellIn < 5) {
+        item.quality = Math.min(50, item.quality + 3)
+        item.sellIn--
+      }
+    }
   }
-  
+  updateConjured(item) {
+    item.quality = Math.max(0, item.quality - 2)
+    item.sellIn--
+    if (item.sellIn < 0) {
+      item.quality = Math.max(0, item.quality - 4)
+      item.sellIn--
+    }
+  }
+  udateNormalItem(item) {
+    item.quality = Math.max(0, item.quality - 1)
+    item.sellIn-- 
+    if (item.sellIn < 0) {
+      item.quality = Math.max(0, item.quality - 2)
+      item.sellIn--
+    }
+  }
+}
+module.exports = {
+  Item,
+  Shop
+}
